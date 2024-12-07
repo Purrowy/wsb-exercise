fa767b9dfda4507047678d2cfe51324fcffde6f0
Purrowy <purr@pc.home>
2024-12-07 13:45:20 +0100

    added multiplication to ex1

--- ex1.py
+++ ex1.py
@@ -2,5 +2,9 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
+# operations on ints
+add = num1 + num2
+product = num1 * num2
+
 # print output
-print(num1 + num2)
+print(f"sum: {add} \nproduct: {product}")
