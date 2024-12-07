bb6d89b63f39d9c2ae9d3e92bbb460449bfc4878
Purrowy <purr@pc.home>
2024-12-07 14:16:04 +0100

    created ex2

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# create list
+fruits = ["banana", "apple", "orange"]
+
+# dictionary with prices
+prices = {"banana": 21, "apple": 37, "orange": 21.37}
+
+# print price for selected fruit
+fav = fruits[1]
+print(f"best fruit is {fav} which costs {prices[fav]}")
